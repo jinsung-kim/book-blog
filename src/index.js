5f044038ef97fe932071994238fd49b71bb7a2a4
Jin Kim <jinsungkim1210@gmail.com>
2025-07-27 20:19:55 -0400

    Always track person profiles

--- src/index.js
+++ src/index.js
@@ -2,20 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import posthog from './utils/posthogClient';
 import { PostHogProvider } from 'posthog-js/react';
-
-const options = {
-  api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST,
-};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <PostHogProvider
-      apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY}
-      options={options}
-    >
+    <PostHogProvider client={posthog}>
       <App />
     </PostHogProvider>
   </React.StrictMode>,
--- src/utils/posthogClient.ts
+++ src/utils/posthogClient.ts
@@ -3,10 +3,25 @@ import { PostHog } from 'posthog-js';
 let posthog: PostHog | undefined = undefined;
 
 if (typeof window !== 'undefined') {
-  // Only import dynamically in browser to ensure React context is ready
+  // Only import dynamically in browser to ensure React context is ready.
   const client = require('posthog-js');
   posthog = client.default;
+
+  posthog.init(process.env.REACT_APP_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+    person_profiles: 'always',
+    loaded: ph => {
+      if (process.env.NODE_ENV === 'development') {
+        ph.debug();
+      }
+    },
+  });
+
+  // Avoid re-initializing.
+  (posthog as any).__loaded = true;
 }
+
+export default posthog;
 
 export function captureEvent(name: string, props?: Record<string, any>) {
   if (posthog) {
